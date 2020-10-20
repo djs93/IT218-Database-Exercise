f9fa9422a41b3f1e837e7de173d19591dd0934c5
Dale <djs93@njit.edu>
2020-10-20 18:56:07 -0400

    Initial commit

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
+    include 'pdo.php';
 
+    try{
+        $query = 'SELECT * FROM products';
+        $statement = $db->prepare($query);
+
+        $statement->execute();
+        $products = $statement->fetchAll();
+        $statement->closeCursor();
+
+        $cat_query = 'SELECT * FROM categories';
+        print_r($products);
+    } catch (Exception $error){
+        $error_message = $error->getMessage();
+        echo "Error running SQL Query: $error_message";
+    }
 ?>
 
 <h1>PDO Demo</h1>
+
+<table border="1">
+    <tr>
+        <th>Product Code</th>
+        <th>Prpodcue Name</th>
+        <th>L:ist Psrice</th>
+    </tr>
+    <?php foreach ($products as $product):?>
+    <tr>
+        <td><?php echo $product['productCode'];?></td>
+        <td><?php echo $product['productName'];?></td>
+        <td><?php echo $product['listPrice'];?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
---
+++ pdo.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = 'sql1.njit.edu';
+$username = 'djs93';
+$password = 'oTB64*ICvje0KFd$G92L';
+$dsn = "mysql:host=$hostname;db=$username";
+$db = '';
+try{
+    $db = new PDO($dsn, $username, $password);
+    echo "connected to sql";
+} catch (PDOException $error){
+    $error_message = $error->getMessage();
+    print "Error Connecting to MySQL: $error_message";
+}
